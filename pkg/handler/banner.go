7fadae32164d4f00706b8fce1f82c31db012f8d7
iGOSTFOST <alekseygostkin@gmail.com>
2024-04-12 00:26:27 +0300

    added the ability to get the latest version when using_last_revision in a request

--- pkg/handler/banner.go
+++ pkg/handler/banner.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"database/sql"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/igostfost/avito_backend_trainee/pkg/types"
 	"net/http"
@@ -10,10 +12,7 @@ import (
 )
 
 func (h *Handler) CreateBannerHandler(c *gin.Context) {
-	//_, ok := c.Get("userId")
-	//if !ok {
-	//	NewErrorResponse(c, http.StatusInternalServerError, "user_id not found in context")
-	//}
+
 	isAdmin, ok := c.Get("isAdmin")
 	if !isAdmin.(bool) || !ok {
 		NewErrorResponse(c, http.StatusForbidden, "Пользователь не имеет доступа - Только администраторы могут создавать баннеры")
@@ -54,19 +53,17 @@ func (h *Handler) GetBannersHandler(c *gin.Context) {
 	if err != nil {
 		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
+	if banners == nil {
+		NewErrorResponse(c, http.StatusNotFound, "Баннер не найден")
+		return
+	}
 
 	c.JSON(http.StatusOK, banners)
 }
 
-func (h *Handler) GetUserBannerHandler(c *gin.Context) {
+func (h *Handler) GetUserBannerFromDB(c *gin.Context, TagId, FeatureId int) {
 
-	var input types.GetInputBanners
-	if err := c.BindJSON(&input); err != nil {
-		NewErrorResponse(c, http.StatusBadRequest, "Некорректные данные")
-		return
-	}
-
-	userBanner, err := h.utils.GetUserBanner(input.FeatureId, input.TagIds)
+	userBanner, err := h.utils.GetUserBanner(FeatureId, TagId)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -77,6 +74,73 @@ func (h *Handler) GetUserBannerHandler(c *gin.Context) {
 		return
 	}
 
+	fmt.Println("получили данные напрямую из дб")
+	c.JSON(http.StatusOK, userBanner)
+}
+
+func (h *Handler) GetUserBannerHandler(c *gin.Context) {
+	// Привязываем параметры из URL запроса
+	var input types.GetInputBanners
+	if err := c.BindJSON(&input); err != nil {
+		NewErrorResponse(c, http.StatusBadRequest, "Некорректные данные")
+		return
+	}
+	if input.UseLastRevision {
+		h.GetUserBannerFromDB(c, input.TagIds, input.FeatureId)
+		// fmt.Println("Ушли получать напрямую из бд")
+		return
+	}
+
+	// Формируем ключ для кеша Redis
+	cacheKey := fmt.Sprintf("user_banner:%d:%d", input.FeatureId, input.TagIds)
+
+	// Проверяем, есть ли данные в кеше Redis
+	cachedData, err := h.utils.Get(c, cacheKey)
+	if err == nil {
+		// Данные найдены в кеше, возвращаем их пользователю
+		var userBanner types.Content
+		cachedDataString, ok := cachedData.(string)
+		if !ok {
+			// Обработка ошибки, если cachedData не является строкой
+			NewErrorResponse(c, http.StatusInternalServerError, "Ошибка при получении данных из кеша")
+			return
+		}
+		if err := json.Unmarshal([]byte(cachedDataString), &userBanner); err != nil {
+			NewErrorResponse(c, http.StatusInternalServerError, "Ошибка при разборе данных из кеша")
+			return
+		}
+		// fmt.Println("Считали из кеша")
+		c.JSON(http.StatusOK, userBanner)
+		return
+	}
+
+	// Данных нет в кеше, получаем их из базы данных
+	userBanner, err := h.utils.GetUserBanner(input.FeatureId, input.TagIds)
+	// fmt.Println("Данных нет в кеше, получаем их из базы данных")
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			NewErrorResponse(c, http.StatusNotFound, "Баннер не найден 1")
+		default:
+			NewErrorResponse(c, http.StatusInternalServerError, "Внутренняя ошибка сервера")
+		}
+		return
+	}
+
+	// Сохраняем данные в кеш Redis
+	jsonData, err := json.Marshal(userBanner)
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, "Ошибка при сериализации данных")
+		return
+	}
+
+	if err := h.utils.Set(c, cacheKey, string(jsonData)); err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, "Ошибка при сохранении данных в кеш")
+		return
+	}
+	// fmt.Println("Сохранили данные в кеш")
+
+	// Возвращаем данные пользователю
 	c.JSON(http.StatusOK, userBanner)
 }
 
