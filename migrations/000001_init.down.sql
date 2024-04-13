16a70f8f1af174e6733a46b195508e8b759f54bb
iGOSTFOST <alekseygostkin@gmail.com>
2024-04-13 13:17:02 +0300

    added migrate bd

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,15 @@
+
+DROP TRIGGER IF EXISTS update_banner_updated_at_trigger ON banners;
+
+
+ALTER TABLE banner_tags DROP CONSTRAINT IF EXISTS banner_tags_banner_id_fkey;
+ALTER TABLE banner_tags DROP CONSTRAINT IF EXISTS banner_tags_tag_id_fkey;
+
+
+DROP TABLE IF EXISTS banner_tags;
+DROP TABLE IF EXISTS banners;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tags;
+
+
+DROP FUNCTION IF EXISTS update_updated_at_column() CASCADE;
