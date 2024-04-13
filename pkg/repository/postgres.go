da8ced3d52cb021ae6f65120c3910548f5a670f8
iGOSTFOST <alekseygostkin@gmail.com>
2024-04-13 19:53:34 +0300

    fixed SSLMode connection to postgres

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -31,5 +31,20 @@ func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return db, nil
 }
+
+//func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+//	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s",
+//		cfg.Host, cfg.Port, cfg.Username, cfg.DBName,cfg.SSLMode, cfg.Password))
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	err = db.Ping()
+//	if err != nil {
+//		return nil, err
+//	}
+//	return db, nil
+//}
